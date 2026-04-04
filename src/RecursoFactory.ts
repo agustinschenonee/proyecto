079dd18bbae80048abb936c716a9c1cf9a4d9b9b
priiscigaleano <priiscigaleano@gmail.com>
2026-04-04 09:29:07 -0300

    Update RecursoFactory.ts

--- src/RecursoFactory.ts
+++ src/RecursoFactory.ts
@@ -1,16 +1,17 @@
-// src/RecursoFactory.ts
-import { Recurso } from './Recurso';
 import { Sala } from './Sala';
 import { Escritorio } from './Escritorio';
+import { Recurso } from './Recurso';
 
 export class RecursoFactory {
     static crearRecurso(tipo: string, datos: any): Recurso {
-        if (tipo === 'SALA') {
-            return new Sala(datos.id, datos.nombre, datos.capacidad, datos.fechaMantenimiento);
+        // IMPORTANTE: Si en la DB pusiste "Sala", acá debe decir "Sala"
+        switch (tipo.toUpperCase()) {
+            case 'SALA':
+                return new Sala(datos.nombre, datos.capacidad, datos.disponible, 'SALA', datos.imagen_url, datos.mantenimiento);
+            case 'ESCRITORIO':
+                return new Escritorio(datos.nombre, datos.capacidad, datos.disponible, 'ESCRITORIO');
+            default:
+                throw new Error("Tipo de recurso no reconocido.");
         }
-        if (tipo === 'ESCRITORIO') {
-            return new Escritorio(datos.id, datos.nombre, datos.capacidad);
-        }
-        throw new Error("Tipo de recurso no reconocido");
     }
 }
