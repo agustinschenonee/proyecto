2e84502c97d924359797a041db6b81073f7c5f86
priiscigaleano <priiscigaleano@gmail.com>
2026-04-04 09:23:30 -0300

    Create Recurso.ts

---
+++ src/Recurso.ts
@@ -0,0 +1,11 @@
+export interface Recurso {
+    id?: number; 
+    nombre: string;
+    capacidad: number;
+    tipo: string;
+    imagen_url?: string;
+    descripcion?: string;
+    disponible: boolean;
+    mantenimiento?: string;
+    obtenerDetalles(): string;
+}
