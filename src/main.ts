705726481a781f28991b6426a38c87be6416405c
priiscigaleano <priiscigaleano@gmail.com>
2026-04-04 10:50:12 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -19,4 +19,3 @@ const emailAlert = new NotificadorEmail();
 emailAlert.actualizar(`La ${miSala.nombre} ya está cargada en el sistema.`);
 
 console.log("--- PRUEBA FINALIZADA ---");
-console.log("--- PRUEBA FINALIZADA ---");
