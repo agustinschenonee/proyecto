f67ef60a7040d8c48eb93d57e7aa128d89ebc503
priiscigaleano <priiscigaleano@gmail.com>
2026-04-04 11:43:35 -0300

    Update supabase.ts

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Reemplazá con tus credenciales reales de Supabase
-const supabaseUrl = 'https://TU_URL.supabase.co';
-const supabaseKey = 'TU_ANON_KEY_LARGA';
+// Datos de tu proyecto ISI-2026-DEVpapois
+const supabaseUrl = 'https://wybiblrhcphwuhrmgkhv.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind5YmlibHJoY3Bod3Vocm1na2h2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQwOTYxOTUsImV4cCI6MjA4OTY3MjE5NX0.qiBG-FqFhI8IaRqlTT-qb10qHQ2SEJrnZ_Djh49VvyA';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
