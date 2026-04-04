0a2822a4e36b43099f7fda28adaa56051559bf28
priiscigaleano <priiscigaleano@gmail.com>
2026-04-04 09:28:40 -0300

    Update Sala.ts

--- src/Sala.ts
+++ src/Sala.ts
@@ -1,15 +1,17 @@
-// src/modules/resources/Sala.ts
 import { Recurso } from './Recurso';
 
 export class Sala implements Recurso {
     constructor(
-        public id: string,
         public nombre: string,
         public capacidad: number,
-        public fechaMantenimiento: string // Viene de tu diagrama
+        public disponible: boolean = true,
+        public tipo: string = 'SALA', // ¡Ojo! Que coincida con la DB
+        public imagen_url?: string,
+        public mantenimiento?: string,
+        public id?: number
     ) {}
 
     obtenerDetalles(): string {
-        return `Sala: ${this.nombre} | Capacidad: ${this.capacidad} personas | Mantenimiento: ${this.fechaMantenimiento}`;
+        return `SALA: ${this.nombre} | Capacidad: ${this.capacidad} | Mantenimiento: ${this.mantenimiento || 'N/A'}`;
     }
 }
