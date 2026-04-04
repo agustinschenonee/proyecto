0e44d655da679448a6f4cb3a1b6bb62f7ada0cac
priiscigaleano <priiscigaleano@gmail.com>
2026-04-04 10:13:15 -0300

    Update Escritorio.ts

--- src/Escritorio.ts
+++ src/Escritorio.ts
@@ -1,19 +1,16 @@
 import { Recurso } from './Recurso';
 
-// 1. Cambiamos el nombre de la clase a Escritorio
 export class Escritorio implements Recurso {
     constructor(
         public nombre: string,
         public capacidad: number,
         public disponible: boolean = true,
-        // 2. El tipo por defecto ahora es 'ESCRITORIO'
-        public tipo: string = 'ESCRITORIO', 
+        public tipo: string = 'ESCRITORIO',
         public imagen_url?: string,
         public id?: number
     ) {}
 
     obtenerDetalles(): string {
-        // 3. Cambiamos el texto para que diga ESCRITORIO
-        return `ESCRITORIO: ${this.nombre} | Capacidad: ${this.capacidad} | Puesto: ${this.id || 'S/N'}`;
+        return `ESCRITORIO: ${this.nombre} | Capacidad: ${this.capacidad} | Puesto ID: ${this.id || 'Nuevo'}`;
     }
 }
