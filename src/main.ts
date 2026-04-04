c11a04781c690f9bc498cd97f2d67702e92c3766
priiscigaleano <priiscigaleano@gmail.com>
2026-04-04 09:36:55 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
 import { RecursoFactory } from './RecursoFactory';
+import { NotificadorEmail } from './Notificadores';
 
-console.log("--- PRUEBA DE SISTEMA COWORKING ---");
+console.log("--- 🏢 SISTEMA DE COWORKING DEVpapois ---");
 
-const sala1 = RecursoFactory.crearRecurso('SALA', {
-    nombre: "Sala Principal",
+// 1. Creamos el recurso usando tu Factory
+const miSala = RecursoFactory.crearRecurso('SALA', {
+    nombre: "Sala Principal Iguazú",
     capacidad: 12,
     disponible: true,
-    mantenimiento: "2026-05-05"
+    mantenimiento: "2026-05-15"
 });
 
-console.log(sala1.obtenerDetalles());
+// 2. Probamos que el objeto se creó bien
+console.log(miSala.obtenerDetalles());
+
+// 3. (Opcional) Mostramos que el Observer está listo para avisar
+const emailAlert = new NotificadorEmail();
+emailAlert.actualizar(`La ${miSala.nombre} ya está cargada en el sistema.`);
