75a2660b2246847141988a342982014254107dde
priiscigaleano <priiscigaleano@gmail.com>
2026-04-04 10:28:49 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import { NotificadorEmail } from './Notificadores'; // Busca en Notificadores.ts
 import { RecursoFactory } from './RecursoFactory';
+import { NotificadorEmail } from './Notificadores';
 
 console.log("--- SISTEMA DE COWORKING DEVpapois ---");
 
-// 1. Creamos una sala usando la Factory
+// 1. Creamos la sala
 const miSala = RecursoFactory.crearRecurso('SALA', {
     nombre: "Sala Principal Iguazú",
     capacidad: 12,
@@ -11,11 +11,11 @@ const miSala = RecursoFactory.crearRecurso('SALA', {
     mantenimiento: "2026-05-15"
 });
 
-// 2. Mostramos los detalles en consola
+// 2. Mostramos detalles
 console.log(miSala.obtenerDetalles());
 
-// 3. Probamos el sistema de notificaciones (Observer)
+// 3. Probamos el Observer
 const emailAlert = new NotificadorEmail();
-emailAlert.actualizar(`El recurso "${miSala.nombre}" ha sido registrado con éxito.`);
+emailAlert.actualizar(`La ${miSala.nombre} ya está cargada en el sistema.`);
 
 console.log("--- PRUEBA FINALIZADA ---");
